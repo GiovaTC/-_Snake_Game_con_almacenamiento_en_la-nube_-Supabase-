2cd4ad72da2ffaaa4f88eb5f145df64630ffe00f
gtapiero <giovanny.tapiero@pi.edu.co>
2025-10-20 12:14:02 -0500

    commit - def subir_puntaje :.

--- main.py
+++ main.py
@@ -10,3 +10,19 @@ pygame.display.set_caption("🐍 Snake con Supabase")
 clock = pygame.time.Clock()
 font = pygame.font.SysFont("consolas", 25)
 
+def subir_puntaje(nombre, puntaje):
+    try:
+        data = {"player": nombre, "score": puntaje}
+        headers = {
+            "apikey": SUPABASE_KEY,
+            "Authorization": f"Bearer {SUPABASE_KEY}",
+            "Content-Type": "application/json"
+        }
+        r = requests.post(f"{SUPABASE_URL}/rest/v1/{SUPABASE_TABLE}", json=data, headers=headers)
+        if r.status_code in (200, 201):
+            print("✅ Puntaje registrado en la nube!")
+        else:
+            print(f"⚠️ Error al registrar puntaje: {r.text}")
+    except Exception as e:
+        print(f"❌ No se pudo conectar a Supabase: {e}")
+
