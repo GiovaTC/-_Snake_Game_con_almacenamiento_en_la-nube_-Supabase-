973bc0b9cf299f87a8fe77b7f3e14acb99af5ba0
gtapiero <giovanny.tapiero@pi.edu.co>
2025-10-20 12:51:09 -0500

    commit - SUPABASE_KEY .

--- supabase_config.py
+++ supabase_config.py
@@ -1,5 +1,5 @@
 # Configuración de conexión 
 
 SUPABASE_URL = "https://wohhwcvrvwogmfnqrxwy.supabase.co"
-SUPABASE_KEY = "@Tapiero123."
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndvaGh3Y3ZydndvZ21mbnFyeHd5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA5NzQzMDcsImV4cCI6MjA3NjU1MDMwN30.d-X1cA3PXaqAS5VnPWsTOLZEO16ajBIsrN_aggr9nQs"
 SUPABASE_TABLE = "scores"
