987e27d0da4b10f030d4513d6fda7e4a75c52f09
gtapiero <giovanny.tapiero@pi.edu.co>
2025-10-20 12:06:40 -0500

    commit - cuatro .

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-# Juego principal
+# Juego principal .
+import pygame, sys, random, requests
+from supabase_config import SUPABASE_URL, SUPABASE_KEY, SUPABASE_TABLE
+
+pygame.init()
+WIDTH, HEIGHT = 600, 400
+CELL = 20
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("🐍 Snake con Supabase")
+clock = pygame.time.Clock()
+font = pygame.font.SysFont("consolas", 25)
+
